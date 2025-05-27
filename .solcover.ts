c74dda1c8a4d9fb36854a2c29c3475c630267205
hbdgr <hbdgr@protonmail.com>
2025-05-27 16:15:38 +0200

    fix coverage tests timeouts

--- .solcover.ts
+++ .solcover.ts
@@ -3,6 +3,7 @@ module.exports = {
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true,              // Run the grep's inverse set.
-    enableTimeouts: false
+    enableTimeouts: false,
+    timeout: 0
   }
 };
